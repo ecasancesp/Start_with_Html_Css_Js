7d2de8fcad3d3a9be8a10b2c0299d8d2270d0fa6
Enrique Cases <ecsvy@hotmail.com>
2016-07-04 09:48:27 +0200

    commitFinal

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log ("Casa");
+$(document).ready(function () {
+
+    "use strict"; /* don't allow undefined variables */
+
+    $("#submit").on("click", function() {
+        var uIdea = $("#userIdea").val();
+        var uDescrip = $("#userDescription").val();
+        var lenIdea = $("#userIdea").val().length;
+        var lenDesc = $("#userDescription").val().length;
+        var save = "Saving...";
+
+        if (lenIdea == 0) {
+            if (lenDesc < 100) {
+                alert("Idea missing \nCheck description requirement");
+            }
+            else{
+                alert("Idea missing");
+            }
+        }
+        else{
+            if (lenDesc < 100) {
+                alert("Check description requirement");
+            }
+            else{
+                $("#saveEnd").val(save).show();
+                $("#saveEnd").val(save).hide(6000);
+            }
+        }
+    });
+});

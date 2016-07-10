b9e28c9217eb8adeaf4b1d749bbab26a397f205d
Enrique Cases <ecsvy@hotmail.com>
2016-07-10 18:58:31 +0200

    Final_2

--- Angular/main.js
+++ Angular/main.js
@@ -1,1 +1,36 @@
 /* Angular */
+
+(function(angular) {    //no es necesario poner 'angular'
+    "use strict";
+    
+    var enriqueApp = angular.module("enriqueApp",[]);
+
+    enriqueApp.controller("DivController", DivController);
+    function DivController($scope) {
+    	$scope.showMe = false;
+        var fin = $scope.arrayText = [];
+        $scope.submitData = function () {
+            var idea = $scope.ideaText;
+            var desc = $scope.descText;
+            if (idea == "" || idea == undefined) {
+                if (desc == undefined || desc == "") {
+                    window.alert("Idea missing \nCheck description requirement");
+                }
+                else {
+                    window.alert("Idea missing");
+                }
+            }
+            else{
+                if (desc == undefined || desc == "") {
+                    window.alert("Check description requirement");
+                }
+                else {
+                    $scope.arrayText.push({idea,desc});
+                    $scope.list = fin;
+                    $scope.showMe = true;
+                }
+            }
+        }
+    }
+    
+})(window.angular); //no es necesario poner 'window.angular'
